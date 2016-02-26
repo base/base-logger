3852d2d5b1f20a9958e7b189340f7ea29730e36d
Brian Woodward <brian.woodward@gmail.com>
2016-02-26 10:47:05 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-logger <https://github.com/node-base/base-logger>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function() {
+};
+
